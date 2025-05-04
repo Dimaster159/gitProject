be201cb6252b5bb82a74029f07b336ea6620a3ed
Dmitriy Pastuhov <remsma702@yandex.ru>
2025-05-05 02:50:39 +0500

    Решение задач на codewars

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-console.log('Привет');
+const str1 = "abcdrf";
+const str2 = "ABCDRF";
+
+function compareStrings(str1, str2) {
+
+    if (str1.length === 0 && str2.length === 0) {
+        return false;
+    }
+
+    if (str1.length !== str2.length) {
+        return false;
+    }
+    if (str1.toUpperCase() === str2.toUpperCase()) {
+        const strToArr1 = str1.split('');
+        const strToArr2 = str2.split('');
+
+        for (i = 0; i < strToArr1.length; i++) {
+            if (strToArr1[i] === strToArr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    return false;
+}
+
+console.log(compareStrings(str1, str2))
+
+function removeExclamationMarks(str) {
+    const chengeStr = str.replaceAll('!', '');
+    return chengeStr;
+}
+
+console.log(removeExclamationMarks('dshf!ds!jdskjf!'))
